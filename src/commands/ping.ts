2f5399a4387988dd02a52305661da3ac5b5e3c51
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-11-24 15:20:40 +0100

    ping doc

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,6 +4,9 @@ import { SlashCommandBuilder } from 'discord.js';
 
 const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
 
+/*
+ * This is the execute function for the ping command.
+ */
 async function execute(interaction: CommandInteraction): Promise<void> {
     await interaction.reply('Pong!');
 }
