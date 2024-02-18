af9043ab8b9c4215cdc891838c8ddc9a00e1e8c4
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-18 01:09:15 +0100

    Use discord min/max range value

--- src/commands/ai/mixtral.ts
+++ src/commands/ai/mixtral.ts
@@ -25,33 +25,48 @@ export const MIXTRAL: Command = {
             option
                 .setName('max_new_tokens')
                 .setDescription('Le nombre maximum de jetons que le modèle doit générer en sortie (1024 par défaut)')
+                .setMinValue(0)
+                .setMaxValue(10000)
                 .setRequired(false),
         )
         .addNumberOption((option) =>
             option
                 .setName('temperature')
                 .setDescription('La valeur utilisée pour moduler les probabilités du prochain token (0.6 par défaut)')
+                .setMinValue(0)
+                .setMaxValue(2)
                 .setRequired(false),
         )
         .addNumberOption((option) =>
             option
                 .setName('top_p')
                 .setDescription('Un seuil de probabilité pour générer la sortie (0.9 par défaut)')
+                .setMinValue(0)
+                .setMaxValue(2)
                 .setRequired(false),
         )
         .addNumberOption((option) =>
             option
                 .setName('top_k')
                 .setDescription("Le nombre de jetons ayant de l'importance (50 par défaut)")
+                .setMinValue(0)
+                .setMaxValue(1000)
                 .setRequired(false),
         )
         .addNumberOption((option) =>
-            option.setName('presence_penalty').setDescription('Pénalité de présence (0 par défaut)').setRequired(false),
+            option
+                .setName('presence_penalty')
+                .setDescription('Pénalité de présence (0 par défaut)')
+                .setMinValue(-2)
+                .setMaxValue(2)
+                .setRequired(false),
         )
         .addNumberOption((option) =>
             option
                 .setName('frequency_penalty')
                 .setDescription('Pénalité de fréquence (0 par défaut)')
+                .setMinValue(-2)
+                .setMaxValue(2)
                 .setRequired(false),
         ),
     async execute(interaction) {
@@ -74,28 +89,6 @@ export const MIXTRAL: Command = {
             frequency_penalty: (interaction.options.get('frequency_penalty')?.value as number) || 0,
             prompt_template: '<s>[INST] {prompt} [/INST] ',
         };
-        if (input.max_new_tokens < 0) {
-            return replyError('Le maximum de jetons est negatif!');
-        } else if (input.max_new_tokens > 10000) {
-            return replyError('Le maximum de jetons est supérieur à 10000, abuses pas frère...');
-        }
-        if (input.temperature < 0 || input.temperature > 2) {
-            return replyError('La valeur de la temperature est invalide! La valeur doit être comprise entre 0 et 2');
-        }
-        if (input.top_p < 0 || input.top_p > 2) {
-            return replyError('La valeur de top_p est invalide! La valeur doit être comprise entre 0 et 2');
-        }
-        // if (input.top_k < 0 || input.top_k > 100) {
-        //     return replyError('La valeur de top_k est invalide!');
-        // }
-        if (input.presence_penalty < -2 || input.presence_penalty > 2) {
-            return replyError('La valeur de presence_penalty est invalide! La valeur doit être comprise entre -2 et 2');
-        }
-        if (input.frequency_penalty < -2 || input.frequency_penalty > 2) {
-            return replyError(
-                'La valeur de frequency_penalty est invalide! La valeur doit être comprise entre -2 et 2',
-            );
-        }
         let msg = '';
         let last_time = Date.now();
         for await (const event of replicate.stream('mistralai/mixtral-8x7b-instruct-v0.1', { input })) {
