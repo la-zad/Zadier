5dd40ebcb985896ceb5fdeaf616a8777cf468bcf
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-20 16:03:56 +0100

    remove magic number

--- src/commands/ai/sdxl_turbo.ts
+++ src/commands/ai/sdxl_turbo.ts
@@ -3,11 +3,13 @@ import { SlashCommandBuilder } from 'discord.js';
 
 import { EventReader } from './hugging_face.ts';
 
+const MAX_SEED_API = 12013012031030;
+
 const DEFAULT_VALUE = {
     strength: 0.7,
     steps: 2,
     get seed(): number {
-        return Math.floor(Math.random() * 12013012031030);
+        return Math.floor(Math.random() * MAX_SEED_API);
     },
 };
 
