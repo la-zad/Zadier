730b1010b909155d3d8510f0896b99d991629b75
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-21 00:35:04 +0100

    Clarify functions

--- src/utils/duration.ts
+++ src/utils/duration.ts
@@ -1,42 +1,42 @@
-const seconds = (sec: number): number => sec * 1000;
-const minutes = (min: number): number => seconds(min * 60);
-const hours = (hrs: number): number => minutes(hrs * 60);
-const days = (da: number): number => hours(da * 24);
+const seconds_to_ms = (sec: number): number => sec * 1000;
+const minutes_to_ms = (min: number): number => seconds_to_ms(min * 60);
+const hours_to_ms = (hrs: number): number => minutes_to_ms(hrs * 60);
+const days_to_ms = (da: number): number => hours_to_ms(da * 24);
 
 export class Duration {
     private constructor(public milliseconds: number) {}
     public static milliseconds = (nb: number): Duration => new Duration(nb);
-    public static seconds = (nb: number): Duration => new Duration(seconds(nb));
-    public static minutes = (nb: number): Duration => new Duration(minutes(nb));
-    public static hours = (nb: number): Duration => new Duration(hours(nb));
-    public static days = (nb: number): Duration => new Duration(days(nb));
+    public static seconds = (nb: number): Duration => new Duration(seconds_to_ms(nb));
+    public static minutes = (nb: number): Duration => new Duration(minutes_to_ms(nb));
+    public static hours = (nb: number): Duration => new Duration(hours_to_ms(nb));
+    public static days = (nb: number): Duration => new Duration(days_to_ms(nb));
 
     public get seconds(): number {
         return this.milliseconds / 1000;
     }
     public set seconds(nb: number) {
-        this.milliseconds = seconds(nb);
+        this.milliseconds = seconds_to_ms(nb);
     }
 
     public get minutes(): number {
         return this.seconds / 60;
     }
     public set minutes(nb: number) {
-        this.milliseconds = minutes(nb);
+        this.milliseconds = minutes_to_ms(nb);
     }
 
     public get hours(): number {
         return this.minutes / 60;
     }
     public set hours(nb: number) {
-        this.milliseconds = hours(nb);
+        this.milliseconds = hours_to_ms(nb);
     }
 
     public get days(): number {
         return this.hours / 24;
     }
     public set days(nb: number) {
-        this.milliseconds = days(nb);
+        this.milliseconds = days_to_ms(nb);
     }
 
     public toString(): string {
