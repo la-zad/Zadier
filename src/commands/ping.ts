973d41427f21dffcc63067e0606da24f20665d7b
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-02-20 21:36:33 +0100

    feat(command): more ping outcomes

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -9,6 +9,16 @@ import { SlashCommandBuilder } from 'discord.js';
 export const PING: Command = {
     data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        const luck = Math.random();
+        switch (true) {
+            case luck < 0.01:
+                await interaction.reply('Shit I missed, well done!');
+                break;
+            case luck < 0.2:
+                await interaction.reply('紅龍スマッシュ!');
+                break;
+            default:
+                await interaction.reply('Pong!');
+        }
     },
 };
