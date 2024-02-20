c50e57cb1b37bf21bc68d8ae23249d5feb531766
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-20 17:23:45 +0100

    Factorize code

--- src/events/dyn_vocal/on_connect.ts
+++ src/events/dyn_vocal/on_connect.ts
@@ -10,19 +10,16 @@ import { ChannelType } from 'discord.js';
 export const DYN_VOCAL_ON_CONNECT: BotEvent = {
     name: 'Dynamic vocal channel - on connect',
     listenTo: 'voiceStateUpdate',
-    async execute(_, newVoiceState) {
-        if (newVoiceState.channelId === null || newVoiceState.guild == null) return;
-        const guild = newVoiceState.guild;
-        const channel = newVoiceState.channel;
-        if (!channel || channel.name.startsWith('Nouveau')) return;
-        const member = newVoiceState.member;
-        if (!member) return;
+    async execute(_, { channel, guild, member }) {
+        if (!channel || channel.name.startsWith('Nouveau') || !member) return;
+
         const chan = await guild.channels.create({
             type: ChannelType.GuildVoice,
             name: `${member.nickname ?? member.user.username} qui parle`,
             parent: channel.parent,
             position: channel.position,
         });
+
         await member.voice.setChannel(chan);
     },
     once: false,
--- src/events/dyn_vocal/on_disconnect.ts
+++ src/events/dyn_vocal/on_disconnect.ts
@@ -9,13 +9,10 @@ import type { BotEvent } from '@events';
 export const DYN_VOCAL_ON_DISCONNECT: BotEvent = {
     name: 'Dynamic vocal channel - on disconnect',
     listenTo: 'voiceStateUpdate',
-    async execute(oldStateChannel, _) {
-        if (oldStateChannel.channelId === null || oldStateChannel.guild == null) return;
-        const channel = oldStateChannel.channel;
-        if (!channel || !channel.name.startsWith('Nouveau')) return;
-        if (channel.members.size === 0) {
-            await channel.delete();
-        }
+    async execute({ channel }, _) {
+        if (!channel || !channel.name.startsWith('Nouveau') || channel.members.size) return;
+
+        await channel.delete();
     },
     once: false,
 };
