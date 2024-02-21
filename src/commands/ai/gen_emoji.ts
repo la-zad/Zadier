9e0a7eb1da7613cbbc9ed34d7dc853d208aab696
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-21 18:15:53 +0100

    transform imageimg attach into emoji

--- src/commands/ai/gen_emoji.ts
+++ src/commands/ai/gen_emoji.ts
@@ -4,6 +4,15 @@ import { SlashCommandBuilder } from 'discord.js';
 import Jimp from 'jimp';
 
 import { DEFAULT_VALUE, EventReader } from './hugging_face.ts';
+import { sleep } from 'bun';
+
+function shrink_image(image: Jimp): Jimp {
+    if (image.getWidth() > 256 && image.getHeight() > 256) {
+        const scale = 256 / (image.getWidth() > image.getHeight() ? image.getWidth() : image.getHeight());
+        image = image.scale(scale);
+    }
+    return image.quality(90);
+}
 
 /*
  * @command     - gen_emoji
@@ -12,36 +21,52 @@ import { DEFAULT_VALUE, EventReader } from './hugging_face.ts';
  */
 export const GEN_EMOJI: Command = {
     data: new SlashCommandBuilder()
-        .setName('gen_emoji')
+        .setName('make_emoji')
         .setDescription(`Génère des emojis avec SDLXL Turbo! (temporaire, ${DURATION_TMP_EMOJI.toString()})`)
-        .addStringOption((option) => option.setName('prompt').setDescription('Le prompt').setRequired(true))
+        .addStringOption((option) =>
+            option.setName('ai_prompt').setDescription('Prompt de génération par IA').setRequired(false),
+        )
+        .addAttachmentOption((option) =>
+            option.setName('image').setDescription('Image à mettre en emoji').setRequired(false),
+        )
         .addNumberOption((option) =>
-            option.setName('seed').setDescription('La graine (aléatoire par défaut)').setRequired(false),
+            option.setName('ai_seed').setDescription('La graine (aléatoire par défaut)').setRequired(false),
         ),
     async execute(interaction) {
         await interaction.deferReply();
-        const hf_options = {
-            ...DEFAULT_VALUE,
-            prompt: interaction.options.get('prompt', true).value as string,
-            seed: (interaction.options.get('seed')?.value as number) ?? DEFAULT_VALUE.seed,
-        };
+
+        const img_attach = interaction.options.get('image')?.attachment;
+        let image = null;
         const guild = interaction.guild;
-        if (!guild) {
-            throw 'La commande doit être utilisée dans un serveur.';
+        if (!guild) throw 'La commande doit être utilisée dans un serveur.';
+
+        if (img_attach) {
+            const resp = await fetch(img_attach.url);
+            if (!resp) throw "Un problème est survenu lors de la récupération de l'image...";
+
+            image = Buffer.from(await resp.arrayBuffer());
+        } else {
+            const hf_options = {
+                ...DEFAULT_VALUE,
+                prompt: interaction.options.get('ai_prompt', true).value as string,
+                seed: (interaction.options.get('seed')?.value as number) ?? DEFAULT_VALUE.seed,
+            };
+            const img_generated = await EventReader.generateImage(hf_options);
+            if (!img_generated) throw "l'image n'a pas pu être générée.";
+
+            image = img_generated.attachment;
         }
-        const image = await EventReader.generateImage(hf_options);
-        if (!image) {
-            throw "l'image n'a pas pu être générée.";
-        }
-        const jimp_image = await Jimp.read(image.attachment);
-        const b64image = await jimp_image.quality(90).getBufferAsync('image/jpeg');
-        const now = Date.now();
+        if (!image) throw 'Un problème est survenu...';
+        const jimp_image = await Jimp.read(image);
+        const shrunk_image = shrink_image(jimp_image);
+        const buffer_image = await shrunk_image.getBufferAsync('image/jpeg');
         const emoji_options = {
-            name: `tmp_${now}`,
-            attachment: `data:image/jpeg;base64,${b64image.toString('base64')}`,
+            name: `tmp_${Date.now()}`,
+            attachment: `data:image/jpeg;base64,${buffer_image.toString('base64')}`,
         };
         const new_emoji = await guild.emojis.create(emoji_options);
         setTimeout(() => void new_emoji.delete(), DURATION_TMP_EMOJI.milliseconds);
+        await sleep(1000);
         await interaction.editReply({
             content: new_emoji.toString(),
         });
