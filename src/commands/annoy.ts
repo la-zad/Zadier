c39ff098992ff0614575f060b01d663020b5da72
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-02-21 20:55:13 +0100

    feat(command): annoy command

---
+++ src/commands/annoy.ts
@@ -0,0 +1,31 @@
+import type { Command } from '@commands';
+import { SlashCommandBuilder } from 'discord.js';
+
+import { isGuildMember, isRole } from '../utils/apiDiscriminent';
+
+/**
+ * @command     - annoy
+ * @description - Ghostping someone
+ * @permission  - None
+ */
+export const ANNOY: Command = {
+    data: new SlashCommandBuilder()
+        .setName('annoy')
+        .setDescription('Ghostping someone')
+        .addMentionableOption((option) =>
+            option.setName('ping').setDescription('The role or User you want to piss off').setRequired(true),
+        ),
+    async execute(interaction) {
+        const ping = interaction.options.get('ping', true);
+
+        if (ping.member && isGuildMember(ping.member)) {
+            await interaction.reply({ content: `${ping.member.toString()}`, ephemeral: true });
+        } else if (ping.role && isRole(ping.role)) {
+            await interaction.reply({ content: `${ping.role.toString()}`, ephemeral: true });
+        } else {
+            throw 'Unkown type of ping';
+        }
+
+        return;
+    },
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,6 +3,7 @@ import type { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { ASK_MIXTRAL } from './ai/ask_mixtral';
 import { GEN_EMOJI } from './ai/make_emoji';
 import { SDXL_TURBO } from './ai/sdxl_turbo';
+import { ANNOY } from './annoy';
 import { PING } from './ping';
 
 type SlashCommandDescriptor = SlashCommandBuilder | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>;
@@ -12,4 +13,4 @@ export type Command = {
     execute: (interaction: CommandInteraction) => Promise<void>;
 };
 
-export const COMMANDS: Array<Command> = [PING, SDXL_TURBO, GEN_EMOJI, ASK_MIXTRAL];
+export const COMMANDS: Array<Command> = [PING, SDXL_TURBO, GEN_EMOJI, ANNOY, ASK_MIXTRAL];
