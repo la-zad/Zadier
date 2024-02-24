bed8108fcb530d01a626e7bb5def0b6359a700dc
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-24 18:54:17 +0100

    Fix Discord message overflow when using zadier (#14)
    * control discord message overflow
    
    * set mixstral model into a constant on top of the file
    
    * prevent sending empty message
    
    * fix shrink message
    
    * reduce default mixttral max new token
    
    * move all mixtral constant on top
    
    * move shrink_text into @utils/text
    
    * fix format
    
    * clarify the partition text function
    
    * feat(improve): `partition_text` (#16)
    
    * feat(improve?): `partition_text`
    
    * add one more test
    
    * more tests. CI. fix edge cases
    
    * make function outside of function
    
    * defer await
    
    * refactor function into class
    
    * improve refacto
    
    ---------
    
    Co-authored-by: Victor LEFEBVRE <dev@vic1707.xyz>

--- src/utils/replicate/mixtral_api.ts
+++ src/utils/replicate/mixtral_api.ts
@@ -1,6 +1,21 @@
-import type { CacheType, CommandInteraction } from 'discord.js';
+import { partition_text, PARTITIONING_PATTERNS } from '@utils/text';
+import type { CacheType, CommandInteraction, Message } from 'discord.js';
 
 import { REPLICATE } from '.';
+
+const MODEL = 'mistralai/mixtral-8x7b-instruct-v0.1';
+
+const MAX_MESSAGE_LENGTH = 2000;
+
+export const DEFAULT_MIXTRAL_VALUES = {
+    max_new_tokens: 512,
+    temperature: 0.6,
+    top_p: 0.9,
+    top_k: 50,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+    prompt_template: '<s>[INST] {prompt} [/INST] ',
+};
 
 export interface InputType {
     prompt: string;
@@ -13,27 +28,65 @@ export interface InputType {
     prompt_template: string;
 }
 
-export const DEFAULT_MIXTRAL_VALUES = {
-    max_new_tokens: 1024,
-    temperature: 0.6,
-    top_p: 0.9,
-    top_k: 50,
-    presence_penalty: 0,
-    frequency_penalty: 0,
-    prompt_template: '<s>[INST] {prompt} [/INST] ',
-};
+function isCommandInteraction(value: unknown): value is CommandInteraction {
+    return (value as CommandInteraction).editReply !== undefined;
+}
 
-export async function execute(interaction: CommandInteraction<CacheType>, input: InputType): Promise<void> {
-    let msg = '';
-    let last_time = Date.now();
-    for await (const event of REPLICATE.stream('mistralai/mixtral-8x7b-instruct-v0.1', { input })) {
-        if (event.event === 'output') {
-            msg += event.data;
-        }
-        if (Date.now() - last_time > 500 && msg !== '') {
-            await interaction.editReply(msg);
-            last_time = Date.now();
+class Sender {
+    static TICK_MILLISECONDS = 500;
+    private last_time = Date.now();
+    private msg: string = '';
+    private defer_await: Promise<Message<boolean>> | null = null;
+    constructor(public sender: CommandInteraction | Message) {}
+    public append(text: string): void {
+        this.msg += text;
+    }
+    public async send_deferred(now = false): Promise<void> {
+        if (now || Date.now() - this.last_time > Sender.TICK_MILLISECONDS) {
+            this.last_time = Date.now();
+            if (this.msg === '') return;
+
+            await this.send_message();
+
+            await this.manage_overflow();
         }
     }
-    await interaction.editReply(msg);
+    private async send_message(): Promise<void> {
+        if (this.defer_await) await this.defer_await;
+        if (isCommandInteraction(this.sender)) {
+            this.defer_await = this.sender.editReply(this.msg);
+        } else {
+            this.defer_await = this.sender.reply(this.msg);
+        }
+    }
+    async manage_overflow(): Promise<void> {
+        while (this.msg.length > MAX_MESSAGE_LENGTH) {
+            const [message, shrunk] = partition_text(
+                this.msg,
+                MAX_MESSAGE_LENGTH,
+                PARTITIONING_PATTERNS.END_OF_SENTENCE,
+            );
+            this.msg = message;
+            await this.send_message();
+            this.defer_await = null;
+            this.msg = shrunk;
+            //prevent sending empty message
+            const shrunk_sendable = shrunk === '' ? '.' : shrunk;
+            this.sender = await (isCommandInteraction(this.sender)
+                ? this.sender.followUp(shrunk_sendable)
+                : this.sender.reply(shrunk_sendable));
+            this.last_time = Date.now();
+        }
+    }
+}
+
+export async function execute(interaction: CommandInteraction<CacheType>, input: InputType): Promise<void> {
+    const replier = new Sender(interaction);
+    for await (const event of REPLICATE.stream(MODEL, { input })) {
+        if (event.event === 'output') {
+            replier.append(event.data);
+            await replier.send_deferred();
+        }
+    }
+    await replier.send_deferred(true);
 }
